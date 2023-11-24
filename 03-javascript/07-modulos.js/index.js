a79a5509712fa944c02454758afa2b92d6d6d484
Danilo0203 <50729253+Danilo0203@users.noreply.github.com>
2023-11-24 09:47:28 -0600

    reto Modulos completado

--- 03-javascript/07-modulos.js/index.js
+++ 03-javascript/07-modulos.js/index.js
@@ -3,10 +3,13 @@ import { validarCorreo } from './validacionCorreo.js';
 /* Crea una función que permita verificar si un correo electrónico 
 es valido o invalido. Después utiliza esta función en un archivo diferente. */
 const correo = (correo) => {
-	validarCorreo(correo);
+	validarCorreo(correo)
+		 // Si el correo cumple con la expresión regular, entonces es valido
+		 ? console.log(`El correo ${correo} es valido`)
+		 // Si el correo no cumple con la expresión regular, entonces es invalido
+		 : console.log(`El correo ${correo} es invalido`);
 };
 correo('correo@correo.com'); // El correo correo@correo.com es valido
-correo('correo.com') // El correo correo.com es invalido
+correo('correo.com'); // El correo correo.com es invalido
 correo('correo@correo'); // El correo correo@correo es invalido
 correo('correo@.com'); // El correo correo@.com es invalido
-
--- 03-javascript/07-modulos.js/validacionCorreo.js
+++ 03-javascript/07-modulos.js/validacionCorreo.js
@@ -4,14 +4,7 @@
  * @returns true si el correo es valido, false si es invalido
  */
 export const validarCorreo = (correo) => {
-    // Expresión regular para validar un correo electrónico
+	// Expresión regular para validar un correo electrónico
 	const expRegCorreo = /\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b/;
-    // Si el correo cumple con la expresión regular, entonces es valido
-	if (expRegCorreo.test(correo)) {
-		console.log(`El correo ${correo} es valido`);
-		return;
-        // Si el correo no cumple con la expresión regular, entonces es invalido
-	} else {
-		console.log(`El correo ${correo} es invalido`);
-	}
+	return expRegCorreo.test(correo);
 };
