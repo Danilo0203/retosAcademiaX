8d900e7bb36f7d75371effe40f4da2676164edd3
‘Danilo’ <danilocalderon0203@gmail.com>
2024-02-29 14:53:04 -0600

    añadir carpeta retos dom

---
+++ 04-dom/01-consultas/app.js
@@ -0,0 +1,11 @@
+// Obtener el elemento header
+console.log(document.querySelector("header"));
+
+//Obtener un boton de navegación
+console.log(document.querySelector("a"));
+
+//Obtener una tarjeta
+console.log(document.querySelector(".card"));
+
+// Obtener el elemento footer
+console.log(document.querySelector("footer"));
